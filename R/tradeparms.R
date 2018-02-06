c0d5e781673da89a91eb6c60aeb4da111fbc3caa
sivasunku <siva.sunku@gmail.com>
2018-02-06 17:25:40 +0530

    c0.0.2.6

--- R/tradeparms.R
+++ R/tradeparms.R
@@ -12,16 +12,27 @@
 #' @return list object
 #' @export
 tradeParms <- function(descr){
-  t <- list( incrSlpFlag  = TRUE,   #Incrase the slp according to trl Pct
+  t <- list( instrument   = "SBIN", #Name of the stock
+             incrSlpFlag  = TRUE,   #Incrase the slp according to trl Pct
              pyramidFlag  = FALSE,  #Do you want to add to existing position
-             qty = 80, #trading quantity
-             longTrades  = TRUE,   #If long trades to be permitted
-             shortTrades = TRUE,   #If short trades to be permitted
-             intraday    = FALSE,  #If strategy is intraday
+             qty = 80,              #trading quantity
+             longTrades  = TRUE,    #If long trades to be permitted
+             shortTrades = TRUE,    #If short trades to be permitted
+             
+             
+             pbFlag      = FALSE,  #If Profit booking to be taken
              pbPct       = 1,      #Profit booking percent
+             
+             slpFlag     = FALSE,  #If slp to be considered
              slpPct      = 1,      #Stop loss percent for capital
-             incrSlpPct  = 1,      #Stop loss after taking profit booking
+             
+             trlFlag     = FALSE,  #If trailing to be done
              trlPct      = 1,      #Percent after which slp is reset
+             incrSlpPct  = 1,      #Stop loss after taking profit booking
+             
+             intraday    = FALSE,   #If strategy is intraday
+             idStartTime = "09:15", #Start time for intraday trades
+             idEndTime   = "15:30", #End time for intraday trades
              description = "default"
   )
   return(t)
